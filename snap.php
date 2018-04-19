90244a595d6159b577c9d51a19d321bee7b4aabe
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-19 08:49:31 -0600

    created class and functions

---
+++ snap.php
@@ -0,0 +1,21 @@
+<?php
+class human {
+    protected $numLimbs= 5;
+    protected $numBrains = 5;
+    public $favoriteFood = "ice cream";
+
+    public function getNumLimbs(): int {
+        return($this->numLives);
+    }
+    public function getNumBrains(): int {
+        return $this->numBrains;
+    }
+    public function getFavoriteFood() string {
+    }
+}
+$bob = new Human();
+echo $bob->getNumLimbs() . PHP_EOL;
+echo $bob->get$numBrains() . PHP_EOL;
+echo $bob->getFavoriteFood() . PHP_EOL;
+
+// cry
